985d6a76162956882be570ba41206933be5ba089
ri-ere <asdlias@naver.com>
2023-10-13 02:00:03 +0900

    모든 가능한 슌츠 리스트 찾는 함수 추가
    근데 제대로 작동 안해서 변경해야함....
    - 할일
    1. 스트링 리스트 리스트 만들어서 안쪽 스트링 리스트 하나당 한가지 슌츠 종류 저장하는 방식으로 만들기
    2. 모든 종류 다 찾으면 정렬하고 중복제거해서 반환하기

--- Assets/Resources/Scripts/HandChecker.cs
+++ Assets/Resources/Scripts/HandChecker.cs
@@ -29,14 +29,12 @@ public class HandChecker
         Dictionary<string, int> duplicates = new Dictionary<string, int>();
         foreach (string tile in tiles)
         {
-            string tmp = tile;
-            if(tmp.Length >= 3) tmp = tmp[..2]; //아카도라일수도 있으니 뒤에 r문자 자르기
-            if (duplicates.ContainsKey(tmp))
-                duplicates[tmp]++;
+            if (duplicates.ContainsKey(tile))
+                duplicates[tile]++;
             else
             {
-                duplicates[tmp] = 1;
-                singleTile.Add(tmp);
+                duplicates[tile] = 1;
+                singleTile.Add(tile);
             }
         }
         //커츠 찾는 부분
@@ -54,14 +52,12 @@ public class HandChecker
         Dictionary<string, int> duplicates = new Dictionary<string, int>();
         foreach (string tile in tiles)
         {
-            string tmp = tile;
-            if(tmp.Length >= 3) tmp = tmp[..2]; //아카도라일수도 있으니 뒤에 r문자 자르기
-            if (duplicates.ContainsKey(tmp))
-                duplicates[tmp]++;
+            if (duplicates.ContainsKey(tile))
+                duplicates[tile]++;
             else
             {
-                duplicates[tmp] = 1;
-                singleTile.Add(tmp);
+                duplicates[tile] = 1;
+                singleTile.Add(tile);
             }
         }
         foreach (string tile in singleTile)
@@ -75,51 +71,59 @@ public class HandChecker
         }
         return pair;
     }
+    //슌츠 가능 리스트들
+    public List<string> WhatCanBeSequence(List<string> tiles)
+    {
+        List<string> sequences = new List<string>();
+        List<string> numbers = (from tmp in tiles where tmp.Length >= 2 select tmp[..2]).Distinct().ToList();
+        //첫글자 같다면 두번째 글자가 i번과 i+1 번이 1차이로 같은지, i번과 i+2번이 2차이로 같은지 비교, 같으면 추가
+        for (int i = 0; i < numbers.Count - 2; ++i)
+            if (numbers[i][0] == numbers[i + 1][0] && numbers[i][0] == numbers[i + 2][0])
+                if (numbers[i][1] == numbers[i + 1][1] - 1 && numbers[i][1] == numbers[i + 2][1] - 2)
+                    sequences.Add(numbers[i] + numbers[i + 1] + numbers[i + 2]);
+        return sequences;
+    }
+    //현재 손패에서 가능한 모든 슌츠들
+    public List<string> EverySequence(List<string> tiles)
+    {
+        List<string> possibility = WhatCanBeSequence(tiles);
+        List<string> sequence = new List<string>();
+        int i = possibility.Count;
+        while (i != 0)
+        {
+            tiles.RemoveAt(tiles.IndexOf(possibility[0][..2]));
+            tiles.RemoveAt(tiles.IndexOf(possibility[0][2..4]));
+            tiles.RemoveAt(tiles.IndexOf(possibility[0][4..6]));
+            sequence.Add(possibility[0]);
+            possibility = WhatCanBeSequence(tiles);
+            i = possibility.Count;
+        }
+        return sequence;
+    }
     //슌츠들 이름 리스트에 넣어서 반환
     public List<string> WhatIsSequence(List<string> tiles)
     {
-        List<string> numbers = new List<string>();
-        //문자열 길이가 2이상한 숫자패만 뽑기
-        foreach (string tile in tiles)
-        {
-            string tmp = tile;
-            if (tmp.Length >= 2) 
-            {
-                // tmp = tmp[..2]; 
-                numbers.Add(tmp[..2]); //아카도라일수도 있으니 뒤에 r문자 자르기
-            }
-        }
         List<string> sequence = new List<string>();
+        List<string> numbers = (from tmp in tiles where tmp.Length >= 2 select tmp[..2]).ToList();
         bool isFind;
         do
         {
             isFind = false;
             List<string> tmp = numbers.Distinct().ToList(); //중복제거
             //정렬되어 들어온다는 가정 하에 비교
+            //첫글자 같다면 두번째 글자가 i번과 i+1 번이 1차이로 같은지, i번과 i+2번이 2차이로 같은지 비교
             for (int i = 0; i < tmp.Count-2;++i)
-            {
-                string first = tmp[i];
-                string second = tmp[i+1];
-                string third = tmp[i+2];
-                //첫글자 같은지 비교
-                if (first[0] == second[0] && first[0] == third[0])
-                {
-                    int fi = first[1];
-                    int si = second[1];
-                    int ti = third[1];
-                    string sText = first[0].ToString() + first[1] + second[1] + third[1];
-                    //첫글자 같다면 두번째 글자가 i번과 i+1 번이 1차이로 같은지, i번과 i+2번이 2차이로 같은지 비교 
-                    if ((fi == si - 1) && (fi == ti - 2))
+                if (tmp[i][0] == tmp[i + 1][0] && tmp[i][0] == tmp[i + 2][0])
+                    if (tmp[i][1] == tmp[i + 1][1] - 1 && (tmp[i][1] == (tmp[i + 2][1] - 2)))
                     {
-                        sequence.Add(sText);
-                        numbers.RemoveAt(numbers.IndexOf(first));
-                        numbers.RemoveAt(numbers.IndexOf(second));
-                        numbers.RemoveAt(numbers.IndexOf(third));
+                        //전부 같다면 슌츠 리스트에 넣고 numbers에서 제거해서 다른 슌츠 찾기
+                        sequence.Add(tmp[i] + tmp[i + 1] + tmp[i + 2]);
+                        numbers.RemoveAt(numbers.IndexOf(tmp[i]));
+                        numbers.RemoveAt(numbers.IndexOf(tmp[i+1]));
+                        numbers.RemoveAt(numbers.IndexOf(tmp[i+2]));
                         isFind = true;
                         break;
                     }
-                }
-            }
         } while (isFind);
         return sequence;
     }
