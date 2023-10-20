6447099372ddd86524e854e9d2b89a16f24f749d
ri-ere <asdlias@naver.com>
2023-10-21 03:22:49 +0900

    내 턴 아니면 바로 버리는 코드와 내 턴 일때 5초 대기하는 코드 작성
    슌츠 커츠 또이츠용 테스트코드 밑에 주석으로 처리함,
    gameplay에 update 안에 내턴 아니면 바로 츠모한거 버리는 코드 작성 - 너무 빨리 버려서 기다리는 코드로 변경해야할듯

--- Assets/Resources/Scripts/GamePlay.cs
+++ Assets/Resources/Scripts/GamePlay.cs
@@ -1,5 +1,7 @@
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 public class GamePlay : MonoBehaviour
 {
     private HandChecker _handChecker = new HandChecker();
@@ -8,6 +10,7 @@ public class GamePlay : MonoBehaviour
     private TileDisplay _tileDisplay;
     private GameDirector _gameDirector;
     private PointCalculator _pointCalculator;
+    private TextMeshProUGUI _nowTime;
     private bool oyaWin = false;
     private bool gameEnd = false;
     private int oya;//gamestate로 옮기기
@@ -16,23 +19,28 @@ public class GamePlay : MonoBehaviour
     private List<Player> players = new List<Player>();
     private List<List<string>> hands = new List<List<string>>();
     private List<int> discardNums = new List<int>();
+    private float _userTime = 0;
+    private bool _isMyTurn;
+    private string _nowTsumoTile;
 
-    void Start()
+    private void Start()
     {
         _tileDisplay = GameObject.Find("TileDisplay").GetComponent<TileDisplay>();
         _gameDirector = GameObject.Find("GameDirector").GetComponent<GameDirector>();
         _pointCalculator = GameObject.Find("PointCalculator").GetComponent<PointCalculator>();
+        _nowTime = GameObject.Find("UserTime").GetComponent<TextMeshProUGUI>();
         players = _gameDirector.getPlayers();
         
         oya = _gameDirector.getOya();
         nowWind = _gameDirector.getNowWind();
         nowPlayer = oya;
+        if (nowPlayer == 0) _isMyTurn = true;
+        else _isMyTurn = false;
         dora = tiles.drawDora();//도라
-        makeFirstDora();
+        makeFirstDoraDisplay();
         
-        //손패
-        //makeTestHand();
-        for (int i = 0; i < 4; ++i)//0으로 바꿔야함
+        //손패 만들기
+        for (int i = 0; i < 4; ++i)
         {
             hands.Add(tiles.getFirstHand());
             hands[i] = HandArrange(hands[i]);//손패 정리
@@ -40,58 +48,73 @@ public class GamePlay : MonoBehaviour
             _tileDisplay.handDisplay(hands[i], i);
         }
 
-        _pointCalculator.PlayerRiichiPointChanger(0);
+        _pointCalculator.PlayerRiichiPointChanger(0);//점수 변경 테스트용
     }
-    void Update()
+
+    private void Update()
     {
+        if (_userTime != 0)
+        {
+            //내 턴일때 시행
+            if (_isMyTurn)
+            {
+                _nowTime.text = ((int)_userTime).ToString();
+                //시간이 전부 지났을때 실행
+                if (_userTime < 1.0f)
+                {
+                    dahai(_nowTsumoTile, nowPlayer);
+                    nowPlayer = ++nowPlayer % 4;
+                    if (nowPlayer == 0) _isMyTurn = true;
+                    else _isMyTurn = false;
+                    _userTime = 0;
+                }
+                else
+                {
+                    _userTime -= Time.deltaTime;
+                }
+            }
+            //내 턴이 아닐때 시행
+            else
+            {
+                _nowTime.text = "";
+                
+                dahai(_nowTsumoTile, nowPlayer);
+                nowPlayer = ++nowPlayer % 4;
+                if (nowPlayer == 0) _isMyTurn = true;
+                else _isMyTurn = false;
+                _userTime = 0;
+            }
+            
+        }
+        else
+        {
+            _nowTsumoTile = tiles.tsumo();//타일에서 뽑기
+            hands[nowPlayer].Add(_nowTsumoTile);//손패에 추가
+            hands[nowPlayer] = HandArrange(hands[nowPlayer]);//손패 정리
+            _tileDisplay.tsumoDisplay(_nowTsumoTile, nowPlayer);//츠모한거 오브젝트 생성
+            _userTime = 5.9f;
+        }
+        
+        
+        
+        
+        
         //손패를 받았을때 14개의 패로 승리 가능인지 확인?
-        //
         //if(_gameState.isGameEnd()) Debug.Log("end");
         //타패 체크
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            string tile = tiles.tsumo();//타일에서 뽑기
-            hands[nowPlayer].Add(tile);//손패에 추가
-            hands[nowPlayer] = HandArrange(hands[nowPlayer]);//손패 정리
-            _tileDisplay.tsumoDisplay(tile, nowPlayer);//츠모한거 오브젝트 생성
-
-            
-            //플레이어 0번 아니면 바로 타패
-            if (nowPlayer != 0)
-            {
-                dahai(tile, nowPlayer);
-            }
-            
-            //커츠인거 확인하는 테스트 코드
-            string tripletStr = "";
-            List<string> triplet = _handChecker.WhatIsTriplet(hands[nowPlayer]);
-            foreach (var t in triplet)
-            {
-                tripletStr += t + ", ";
-            }
-            Debug.Log("triplets : " + tripletStr);
-            //또이츠인거 확인하는 테스트 코드
-            string pairStr = "";
-            List<string> pair = _handChecker.WhatIsPair(hands[nowPlayer]);
-            foreach (var p in pair)
-            {
-                pairStr += p + ", ";
-            }
-            Debug.Log("pairs : " + pairStr);
-            //슌츠인거 확인하는 테스트 코드
-            string sequenceStr = "";
-            List<string> sequence = _handChecker.WhatIsSequence(hands[nowPlayer]);
-            foreach (var s in sequence)
-            {
-                sequenceStr += s + ", ";
-            }
-            Debug.Log("sequence : " + sequenceStr);
-            
-            ++nowPlayer;
-            nowPlayer %= 4;
-        }
     }
-    private void makeFirstDora()
+
+    private IEnumerator TimeOverDahai(string tile, int nowPlayer)
+    {
+        Debug.Log("not my turn");
+        yield return new WaitForSeconds(2f);
+        dahai(tile, nowPlayer);
+        nowPlayer = ++nowPlayer % 4;
+        if (nowPlayer == 0) _isMyTurn = true;
+        else _isMyTurn = false;
+        _userTime = 0;
+    }
+    private void makeFirstDoraDisplay()
     {
         _tileDisplay.doraDisplay(dora[0], 0, true);
         _tileDisplay.doraDisplay(dora[1], 1, false);
@@ -99,7 +122,7 @@ public class GamePlay : MonoBehaviour
         _tileDisplay.doraDisplay(dora[3], 3, false);
         _tileDisplay.doraDisplay(dora[4], 4, false);
     }
-    private void makeKanDora(int num)
+    private void makeKanDoraDisplay(int num)
     {
         _tileDisplay.doraDisplay(dora[num], num, true);
         
@@ -195,3 +218,28 @@ public class GamePlay : MonoBehaviour
         Debug.Log(tileToPrint);
     }
 }
+
+// //커츠인거 확인하는 테스트 코드
+// string tripletStr = "";
+// List<string> triplet = _handChecker.WhatIsTriplet(hands[nowPlayer]);
+// foreach (var t in triplet)
+// {
+//     tripletStr += t + ", ";
+// }
+// Debug.Log("triplets : " + tripletStr);
+// //또이츠인거 확인하는 테스트 코드
+// string pairStr = "";
+// List<string> pair = _handChecker.WhatIsPair(hands[nowPlayer]);
+// foreach (var p in pair)
+// {
+//     pairStr += p + ", ";
+// }
+// Debug.Log("pairs : " + pairStr);
+// //슌츠인거 확인하는 테스트 코드
+// string sequenceStr = "";
+// List<string> sequence = _handChecker.WhatIsSequence(hands[nowPlayer]);
+// foreach (var s in sequence)
+// {
+//     sequenceStr += s + ", ";
+// }
+// Debug.Log("sequence : " + sequenceStr);
