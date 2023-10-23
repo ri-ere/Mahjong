50ae0b167bcfb04e31fefbd2f74fe54fcfcfb67f
ri-ere <51537267+ri-ere@users.noreply.github.com>
2023-10-23 23:12:03 +0900

    리치 버튼 자잘한 수정

--- Assets/Resources/Scripts/RiichiButton.cs
+++ Assets/Resources/Scripts/RiichiButton.cs
@@ -2,16 +2,17 @@ using UnityEngine;
 
 public class RiichiButton : MonoBehaviour
 {
-    
+    private GameObject _riichiButton;
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
+        _riichiButton = GameObject.Find("RiichiButton");
+        // _riichiButton.SetActive(false);
         
     }
-
-    // Update is called once per frame
-    void Update()
+    
+    public void RiichiBtnClicked()
     {
-        
+        Debug.Log("riichi button clicked");
     }
 }
