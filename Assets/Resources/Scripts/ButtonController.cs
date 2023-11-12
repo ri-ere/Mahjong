10e7d672e630630993c534590078afe0fcfd8a02
ri-ere <asdlias@naver.com>
2023-11-13 01:35:38 +0900

    버튼 켜고 끄는 함수 추가
    버튼 눌렀을때 작동하는 함수 수정해야함

--- Assets/Resources/Scripts/ButtonController.cs
+++ Assets/Resources/Scripts/ButtonController.cs
@@ -4,27 +4,83 @@ using UnityEngine;
 
 public class ButtonController : MonoBehaviour
 {
-    private GameObject _riichiButton;
+    private GameObject _chiButton;
+    private GameObject _pongButton;
+    private GameObject _kanButton;
     private GameObject _ronButton;
+    private GameObject _riichiButton;
     private GamePlay _gamePlay;
     // Start is called before the first frame update
     private void Start()
     {
-        _riichiButton = GameObject.Find("RiichiButton");
+        _chiButton = GameObject.Find("ChiButton");
+        _pongButton = GameObject.Find("PongButton");
+        _kanButton = GameObject.Find("KanButton");
         _ronButton = GameObject.Find("RonButton");
-        _riichiButton.SetActive(false);
+        _riichiButton = GameObject.Find("RiichiButton");
+        _chiButton.SetActive(false);
+        _pongButton.SetActive(false);
+        _kanButton.SetActive(false);
         _ronButton.SetActive(false);
+        _riichiButton.SetActive(false);
 
+    }
+
+    public void ChiBtnClicked()
+    {
+        _gamePlay = GameObject.Find("GamePlay(Clone)").GetComponent<GamePlay>();
+        Debug.Log("Chi button clicked");
+        _gamePlay.GameEnd();
+    }
+    public void PongBtnClicked()
+    {
+        _gamePlay = GameObject.Find("GamePlay(Clone)").GetComponent<GamePlay>();
+        Debug.Log("Pong button clicked");
+        _gamePlay.GameEnd();
+    }
+    public void KanBtnClicked()
+    {
+        _gamePlay = GameObject.Find("GamePlay(Clone)").GetComponent<GamePlay>();
+        Debug.Log("Kan button clicked");
+        _gamePlay.GameEnd();
     }
     public void RonBtnClicked()
     {
         _gamePlay = GameObject.Find("GamePlay(Clone)").GetComponent<GamePlay>();
-        Debug.Log("ron button clicked");
+        Debug.Log("Ron button clicked");
         _gamePlay.GameEnd();
     }
     public void RiichiBtnClicked()
     {
         _gamePlay = GameObject.Find("GamePlay(Clone)").GetComponent<GamePlay>();
-        Debug.Log("riichi button clicked");
+        Debug.Log("Riichi button clicked");
+    }
+    public void ChiBtnActivate()
+    {
+        _chiButton.SetActive(true);
+    }
+    public void PongBtnActivate()
+    {
+        _pongButton.SetActive(true);
+    }
+    public void KanBtnActivate()
+    {
+        _kanButton.SetActive(true);
+    }
+    public void RonBtnActivate()
+    {
+        _ronButton.SetActive(true);
+    }
+    public void RiichiBtnActivate()
+    {
+        _riichiButton.SetActive(true);
+    }
+    public void AllBtnDeactivate()
+    {
+        _chiButton.SetActive(false);
+        _pongButton.SetActive(false);
+        _kanButton.SetActive(false);
+        _ronButton.SetActive(false);
+        _riichiButton.SetActive(false);
     }
 }
