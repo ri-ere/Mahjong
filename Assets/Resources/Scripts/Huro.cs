28dcad7ffdef49751d591bc5081feff71a1ff350
ri-ere <asdlias@naver.com>
2023-11-17 03:34:03 +0900

    치퐁 하는 함수 생성
    론하는거 만들어야함

--- Assets/Resources/Scripts/Huro.cs
+++ Assets/Resources/Scripts/Huro.cs
@@ -3,12 +3,69 @@ using System.Linq;
 
 public class Huro : HandChecker
 {
-    List<string> DoPong(string huroTile)
+    //CanRon()
+
+
+    List<string> WhatToChi(List<string> hand, string huroTile)
     {
         List<string> result = new List<string>();
+        List<string> numbers = (from tmp in hand where tmp.Length >= 2 select tmp[..2]).Distinct().ToList();
+        const int zero = 48;
+        int num = huroTile[1] - zero;
+        char tileId = huroTile[0];
+        string a;
+        string b;
+        switch (num)
+        {
+            case 1:
+                a = tileId + (num + 1).ToString();
+                b = tileId + (num + 2).ToString();
+                if(numbers.Contains(a) && numbers.Contains(b)) result.Add(huroTile + a + b);
+                break;
+            case 2:
+                a = tileId + (num - 1).ToString();
+                b = tileId + (num + 1).ToString();
+                if(numbers.Contains(a) && numbers.Contains(b)) result.Add(a + huroTile + b);
+                a = tileId + (num + 1).ToString();
+                b = tileId + (num + 2).ToString();
+                if(numbers.Contains(a) && numbers.Contains(b)) result.Add(huroTile + a + b);
+                break;
+            case 8:
+                a = tileId + (num - 2).ToString();
+                b = tileId + (num - 1).ToString();
+                if(numbers.Contains(a) && numbers.Contains(b)) result.Add(a + b + huroTile);
+                a = tileId + (num - 1).ToString();
+                b = tileId + (num + 1).ToString();
+                if(numbers.Contains(a) && numbers.Contains(b)) result.Add(a + huroTile + b);
+                break;
+            case 9:
+                a = tileId + (num - 2).ToString();
+                b = tileId + (num - 1).ToString();
+                if(numbers.Contains(a) && numbers.Contains(b)) result.Add(a + b + huroTile);
+                break;
+            default:
+                a = tileId + (num - 2).ToString();
+                b = tileId + (num - 1).ToString();
+                if(numbers.Contains(a) && numbers.Contains(b)) result.Add(a + b + huroTile);
+                a = tileId + (num - 1).ToString();
+                b = tileId + (num + 1).ToString();
+                if(numbers.Contains(a) && numbers.Contains(b)) result.Add(a + huroTile + b);
+                a = tileId + (num + 1).ToString();
+                b = tileId + (num + 2).ToString();
+                if(numbers.Contains(a) && numbers.Contains(b)) result.Add(huroTile + a + b);
+                break;
+        }
         return result;
     }
-    List<string> DoKan(List<string> huroHand, string huroTile, bool isMyTurn)//huroHand 재구축
+    string MakeChi(string huroTile)
+    {
+        return "s-" + huroTile;
+    }
+    string MakePong(string huroTile)
+    {
+        return "t-" + huroTile + huroTile;
+    }
+    public List<string> DoKan(List<string> huroHand, string huroTile, bool isMyTurn)//huroHand 재구축
     {
         List<string> result = new List<string>();
         string tile = "";
@@ -78,15 +135,20 @@ public class Huro : HandChecker
     }
     List<string> MakeCanChiList(List<string> hand)
     {
-        List<string> result = WhatIsPair(hand).Distinct().ToList();
-        return result;
+        List<string> result = new List<string>();
+        List<string> numbers = hand.Select(tile => tile.Length > 2 ? tile[..2] : tile).ToList();
+        result.AddRange(MChiMaker(numbers));
+        result.AddRange(PChiMaker(numbers));
+        result.AddRange(SChiMaker(numbers));
+
+        return result.Distinct().ToList();
     }
     List<string> MakeCanPongList(List<string> hand)
     {
         List<string> result = WhatIsPair(hand).Distinct().ToList();
         return result;
     }
-    List<string> MakeCanDaiminKanList(List<string> hand)//대명깡, so 커츠 있는거에 추가, 남꺼 가져와서 하는거라 그거 체크 하는거 넣어야할듯
+    List<string> MakeCanDaiminKanList(List<string> hand)//대명깡, so 커츠 있는거에 추가
     {
         List<string> result = WhatIsTriplet(hand).Distinct().ToList();
         return result;
@@ -114,6 +176,148 @@ public class Huro : HandChecker
                 result.Add(pairs[i]);
                 ++i;
             }
+        }
+        return result;
+    }
+    
+    List<string> MChiMaker(List<string> numbers)
+    {
+        List<string> result = new List<string>();
+        //변짱 대기
+        if(numbers.Contains("m1") && numbers.Contains("m2")) result.Add("m3");
+        if(numbers.Contains("m8") && numbers.Contains("m9")) result.Add("m7");
+        //간짱 대기
+        if(numbers.Contains("m1") && numbers.Contains("m3")) result.Add("m2");
+        if(numbers.Contains("m2") && numbers.Contains("m4")) result.Add("m3");
+        if(numbers.Contains("m3") && numbers.Contains("m5")) result.Add("m4");
+        if(numbers.Contains("m4") && numbers.Contains("m6")) result.Add("m5");
+        if(numbers.Contains("m5") && numbers.Contains("m7")) result.Add("m6");
+        if(numbers.Contains("m6") && numbers.Contains("m8")) result.Add("m7");
+        if(numbers.Contains("m7") && numbers.Contains("m9")) result.Add("m8");
+        //양면 대기
+        if(numbers.Contains("m2") && numbers.Contains("m3"))
+        {
+            result.Add("m1");
+            result.Add("m4");
+        }
+        if(numbers.Contains("m3") && numbers.Contains("m4"))
+        {
+            result.Add("m2");
+            result.Add("m5");
+        }
+        if(numbers.Contains("m4") && numbers.Contains("m5"))
+        {
+            result.Add("m3");
+            result.Add("m6");
+        }
+        if(numbers.Contains("m5") && numbers.Contains("m6"))
+        {
+            result.Add("m4");
+            result.Add("m7");
+        }
+        if(numbers.Contains("m6") && numbers.Contains("m7"))
+        {
+            result.Add("m5");
+            result.Add("m8");
+        }
+        if(numbers.Contains("m7") && numbers.Contains("m8"))
+        {
+            result.Add("m6");
+            result.Add("m9");
+        }
+        return result;
+    }
+    List<string> PChiMaker(List<string> numbers)
+    {
+        List<string> result = new List<string>();
+        //변짱 대기
+        if(numbers.Contains("p1") && numbers.Contains("p2")) result.Add("p3");
+        if(numbers.Contains("p8") && numbers.Contains("p9")) result.Add("p7");
+        //간짱 대기
+        if(numbers.Contains("p1") && numbers.Contains("p3")) result.Add("p2");
+        if(numbers.Contains("p2") && numbers.Contains("p4")) result.Add("p3");
+        if(numbers.Contains("p3") && numbers.Contains("p5")) result.Add("p4");
+        if(numbers.Contains("p4") && numbers.Contains("p6")) result.Add("p5");
+        if(numbers.Contains("p5") && numbers.Contains("p7")) result.Add("p6");
+        if(numbers.Contains("p6") && numbers.Contains("p8")) result.Add("p7");
+        if(numbers.Contains("p7") && numbers.Contains("p9")) result.Add("p8");
+        //양면 대기
+        if(numbers.Contains("p2") && numbers.Contains("p3"))
+        {
+            result.Add("p1");
+            result.Add("p4");
+        }
+        if(numbers.Contains("p3") && numbers.Contains("p4"))
+        {
+            result.Add("p2");
+            result.Add("p5");
+        }
+        if(numbers.Contains("p4") && numbers.Contains("p5"))
+        {
+            result.Add("p3");
+            result.Add("p6");
+        }
+        if(numbers.Contains("p5") && numbers.Contains("p6"))
+        {
+            result.Add("p4");
+            result.Add("p7");
+        }
+        if(numbers.Contains("p6") && numbers.Contains("p7"))
+        {
+            result.Add("p5");
+            result.Add("p8");
+        }
+        if(numbers.Contains("p7") && numbers.Contains("p8"))
+        {
+            result.Add("p6");
+            result.Add("p9");
+        }
+        return result;
+    }
+    List<string> SChiMaker(List<string> numbers)
+    {
+        List<string> result = new List<string>();
+        //변짱 대기
+        if(numbers.Contains("s1") && numbers.Contains("s2")) result.Add("s3");
+        if(numbers.Contains("s8") && numbers.Contains("s9")) result.Add("s7");
+        //간짱 대기
+        if(numbers.Contains("s1") && numbers.Contains("s3")) result.Add("s2");
+        if(numbers.Contains("s2") && numbers.Contains("s4")) result.Add("s3");
+        if(numbers.Contains("s3") && numbers.Contains("s5")) result.Add("s4");
+        if(numbers.Contains("s4") && numbers.Contains("s6")) result.Add("s5");
+        if(numbers.Contains("s5") && numbers.Contains("s7")) result.Add("s6");
+        if(numbers.Contains("s6") && numbers.Contains("s8")) result.Add("s7");
+        if(numbers.Contains("s7") && numbers.Contains("s9")) result.Add("s8");
+        //양면 대기
+        if(numbers.Contains("s2") && numbers.Contains("s3"))
+        {
+            result.Add("s1");
+            result.Add("s4");
+        }
+        if(numbers.Contains("s3") && numbers.Contains("s4"))
+        {
+            result.Add("s2");
+            result.Add("s5");
+        }
+        if(numbers.Contains("s4") && numbers.Contains("s5"))
+        {
+            result.Add("s3");
+            result.Add("s6");
+        }
+        if(numbers.Contains("s5") && numbers.Contains("s6"))
+        {
+            result.Add("s4");
+            result.Add("s7");
+        }
+        if(numbers.Contains("s6") && numbers.Contains("s7"))
+        {
+            result.Add("s5");
+            result.Add("s8");
+        }
+        if(numbers.Contains("s7") && numbers.Contains("s8"))
+        {
+            result.Add("s6");
+            result.Add("s9");
         }
         return result;
     }
